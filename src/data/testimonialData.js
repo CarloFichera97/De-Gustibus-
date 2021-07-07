7283c5120e44762004a77b218540fb4e27317518
Carlo Fichera <Carlo.fichera97@gmail.com>
2021-07-07 20:54:01 +0100

    Modified testimonials

--- src/data/testimonialData.js
+++ src/data/testimonialData.js
@@ -14,39 +14,19 @@ const testimonialData = [
     link: "https://www.tripadvisor.com/ShowUserReviews-g187892-d12538208-r727548620-De_Gustibus-Taormina_Province_of_Messina_Sicily.html",
   },
   {
-    title: `TO BE DECIDED`,
-    paragraph: `"TO BE DECIDED"`,
+    title: `The perfect place for a good meal at a fair price`,
+    paragraph: `"Not wanting to spend too much money for a meal, I was very fortunate to find this resturant. The staff is very friendly and the pasta carbonara I tried was delicious. The resturant has a nice atmosphere and is perfect both if you are eating alone or with others. I will definetely visit again."`,
     image: "https://bootdey.com/img/Content/avatar/avatar2.png",
-    author: "Sting",
-    link: "https://www.tripadvisor.com/ShowUserReviews-g187892-d12538208-r727548620-De_Gustibus-Taormina_Province_of_Messina_Sicily.html",
+    author: "F3073simons",
+    link: "https://www.tripadvisor.com/ShowUserReviews-g187892-d12538208-r758304485-De_Gustibus-Taormina_Province_of_Messina_Sicily.html",
   },
   {
-    title: `TO BE DECIDED`,
-    paragraph: `"TO BE DECIDED."`,
+    title: `Perfect stop in Taormina`,
+    paragraph: `"Stepping out of the busy streets and touristy restaurants and finding Rosana and her Bistro was a perfect move. Her selection of local produce and wines is exceptional and gave us a true and excellent taste of Sicily.
+    I would go back many times and highly recommend for the travelers that enjoy authentic food and fair prices."`,
     image: "https://bootdey.com/img/Content/avatar/avatar3.png",
-    author: "Sting",
-    link: "https://www.tripadvisor.com/ShowUserReviews-g187892-d12538208-r727548620-De_Gustibus-Taormina_Province_of_Messina_Sicily.html",
-  },
-  {
-    title: `TO BE DECIDED`,
-    paragraph: `"TO BE DECIDED"`,
-    image: "https://bootdey.com/img/Content/avatar/avatar4.png",
-    author: "Sting",
-    link: "https://www.tripadvisor.com/ShowUserReviews-g187892-d12538208-r727548620-De_Gustibus-Taormina_Province_of_Messina_Sicily.html",
-  },
-  {
-    title: `TO BE DECIDED`,
-    paragraph: `"TO BE DECIDED"`,
-    image: "https://bootdey.com/img/Content/avatar/avatar5.png",
-    author: "Sting",
-    link: "https://www.tripadvisor.com/ShowUserReviews-g187892-d12538208-r727548620-De_Gustibus-Taormina_Province_of_Messina_Sicily.html",
-  },
-  {
-    title: `TO BE DECIDED`,
-    paragraph: `"TO BE DECIDED"`,
-    image: "https://bootdey.com/img/Content/avatar/avatar6.png",
-    author: "Sting",
-    link: "https://www.tripadvisor.com/ShowUserReviews-g187892-d12538208-r727548620-De_Gustibus-Taormina_Province_of_Messina_Sicily.html",
+    author: "oloffredi",
+    link: "https://www.tripadvisor.com/ShowUserReviews-g187892-d12538208-r673027315-De_Gustibus-Taormina_Province_of_Messina_Sicily.html",
   },
 ];
 
