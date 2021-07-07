9a7de12ee9cc94892007087eb4e566f3673d99a9
Carlo Fichera <Carlo.fichera97@gmail.com>
2021-07-07 20:37:30 +0100

    Added Modal setup

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,7 @@
 import React from "react";
 import GalleryElement from "./GalleryElement";
 import galleryData from "./../data/galleryData";
+
 /*
 Looping through the following
 galleryData = [{
@@ -11,18 +12,45 @@ galleryData = [{
   image:"YY"
 }]
 */
-const Gallery = () => {
-  return (
-    <div className="gallery">
-      {galleryData.map((column) => (
-        <div className="gallery__column">
-          {column.map((element) => (
-            <GalleryElement name={element.name} image={element.image} />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
+class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  handleOpenModal = () => {
+    this.setState({
+      showModal: true,
+    });
+  };
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="gallery">
+        {galleryData.map((column) => (
+          <div className="gallery__column">
+            {column.map((element) => (
+              <GalleryElement
+                handleCloseModal={this.handleCloseModal}
+                handleOpenModal={this.handleOpenModal}
+                showModal={this.state.showModal}
+                name={element.name}
+                image={element.image}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Gallery;
--- src/components/GalleryElement.js
+++ src/components/GalleryElement.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const GalleryElement = (props) => {
   return (
-    <a target="_blank" className="gallery__link">
+    <a
+      onClick={props.handleOpenModal}
+      target="_blank"
+      className="gallery__link"
+    >
       <figure className="gallery__thumb">
         <img src={props.image} alt={props.name} className="gallery__image" />
         <figcaption className="gallery__caption">{props.name}</figcaption>
